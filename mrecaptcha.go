ecbc0b16e3dd44e8ab86ed5302e296a74b0a25c9
hromov <melifarowow@gmail.com>
2019-09-27 16:04:25 +0300

    without appengine

--- mrecaptcha.go
+++ mrecaptcha.go
@@ -2,10 +2,7 @@ package mrecaptcha
 
 import (
 	"encoding/json"
-
-	"google.golang.org/appengine"
-	glog "google.golang.org/appengine/log"
-	"google.golang.org/appengine/urlfetch"
+	"log"
 
 	"io/ioutil"
 	"net/http"
@@ -21,8 +18,7 @@ type RecaptchaResponse struct {
 }
 
 func RecaptchaCheck(req *http.Request, secret string, recaptchaResponse string) (bool, error) {
-	c := appengine.NewContext(req)
-	client := urlfetch.Client(c)
+	client := http.Client{}
 	resp, err := client.PostForm("https://www.google.com/recaptcha/api/siteverify",
 		url.Values{
 			"secret":   {secret},
@@ -30,19 +26,19 @@ func RecaptchaCheck(req *http.Request, secret string, recaptchaResponse string) 
 			"remoteip": {req.RemoteAddr},
 		})
 	if err != nil {
-		glog.Errorf(c, "Captcha post error: %+v", err)
+		log.Printf("Captcha post error: %+v", err)
 		return false, err
 	}
 	defer resp.Body.Close()
 	rr := new(RecaptchaResponse)
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		glog.Errorf(c, "Captcha read error: could not read body: %+v", err)
+		log.Printf("Captcha read error: could not read body: %+v", err)
 		return false, err
 	}
 	err = json.Unmarshal(body, &rr)
 	if err != nil {
-		glog.Errorf(c, "Captcha JSON error: %s", err)
+		log.Printf("Captcha JSON error: %s", err)
 		return false, err
 	}
 	return rr.Success, nil
